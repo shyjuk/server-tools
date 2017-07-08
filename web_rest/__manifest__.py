0d3eec72bb0ac5b9f803518aa3e087fa6c792000
Dave Lasley <dave@laslabs.com>
2017-07-08 10:12:22 -0700

    Initial commit

---
+++ web_rest/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Web REST',
+    'summary': 'Provides a REST API authenticated with OAuth2.',
+    'version': '10.0.1.0.0',
+    'category': 'Authentication',
+    'website': 'https://laslabs.com/',
+    'author': 'LasLabs, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'oauth_provider',
+    ],
+}
